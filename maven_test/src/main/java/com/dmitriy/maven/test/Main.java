54304374b21d15e06396c17fdb4e369f975098aa
Dmitriy Tsyhanok <98711244+DmitriyTsi@users.noreply.github.com>
2022-02-18 16:20:43 +0200

    +

---
+++ maven_test/src/main/java/com/dmitriy/maven/test/Main.java
@@ -0,0 +1,15 @@
+package com.dmitriy.maven.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ScannerPoint scan = new ScannerPoint();
+        scan.Scan();
+
+
+
+    }
+
+
+}
